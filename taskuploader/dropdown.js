a71f7ba4af06d6fbff7ca28dec5ee29ab3b47b7e
Subhraneel Saha <56465655+saha350@users.noreply.github.com>
2021-11-28 01:36:00 +0530

    Add files via upload

---
+++ taskuploader/dropdown.js
@@ -0,0 +1,16 @@
+const selected = document.querySelector("#person");
+const optionsContainer = document.querySelector(".person_container");
+
+const optionsList = document.querySelectorAll(".option");
+
+selected.addEventListener("click", () => {
+  optionsContainer.classList.toggle("active");
+});
+
+optionsList.forEach(o => {
+  o.addEventListener("click", () => {
+    selected.innerHTML = o.querySelector("label").innerHTML;
+    optionsContainer.classList.remove("active");
+  });
+});
+
